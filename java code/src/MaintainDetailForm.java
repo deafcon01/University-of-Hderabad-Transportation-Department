26b0d0fd0333f82bc02ea0066bd1fa36ad9219f6
deafcon01 <39852862+deafcon01@users.noreply.github.com>
2018-06-26 12:28:52 +0530

    Update MaintainDetailForm.java

--- java code/src/MaintainDetailForm.java
+++ java code/src/MaintainDetailForm.java
@@ -1,4 +1,8 @@
 
+/**
+ *
+ * @author Rahul
+ */
 import com.mysql.jdbc.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,16 +13,6 @@ import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Rahul
- */
 public class MaintainDetailForm extends javax.swing.JFrame {
 
     /**
