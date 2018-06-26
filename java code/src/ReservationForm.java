f1c4c6356480056dedb1df4f85c9fa6937563257
deafcon01 <39852862+deafcon01@users.noreply.github.com>
2018-06-26 12:33:25 +0530

    Update ReservationForm.java

--- java code/src/ReservationForm.java
+++ java code/src/ReservationForm.java
@@ -1,3 +1,7 @@
+/**
+ *
+ * @author Rahul
+ */
 
 import com.mysql.jdbc.ResultSetMetaData;
 import java.sql.Connection;
@@ -10,16 +14,6 @@ import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Rahul
- */
 public class ReservationForm extends javax.swing.JFrame {
 
     /**
